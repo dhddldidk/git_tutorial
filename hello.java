1b44cd5d26d304294e4698f6464c750f8e57490d
HyeSeon Yang <skcm19@naver.com>
2018-01-30 11:46:03 +0900

    added output "Tell Her World"

--- hello.java
+++ hello.java
@@ -4,4 +4,5 @@ public class HelloGit{
 	public static void main(String[] args){
 		System.out.println("Hello git");
 		System.out.println("Tell Your World");	
+		System.out.println("Tell Her World");	
 	}

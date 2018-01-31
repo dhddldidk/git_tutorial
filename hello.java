71901843476b0f9ecef55fdb18255aff12b6b371
dhddldidk <35945015+dhddldidk@users.noreply.github.com>
2018-01-31 10:48:24 +0900

    github add 1 line

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 package git_test;
 // local modification
 //github modification
+//github add 1 line
 public class HelloGit{
 	public static void main(String[] args){
 		System.out.println("Hello git");
